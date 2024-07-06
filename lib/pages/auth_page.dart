840862e0ebaba197d25e9e58f84e681804cd168a
CJ Seven <hperm9166@gmail.com>
2024-07-07 05:44:41 +0800

    Updated Login UI

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -2,15 +2,40 @@
 import 'package:flutter/material.dart';
 
 // Third-party package imports
-import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
-import 'package:firebase_ui_auth/firebase_ui_auth.dart';
-import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 // Local project imports - Pages
 import 'package:nutsnbolts/pages/route_page.dart';
+import 'package:nutsnbolts/utils/constants.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
+
+  Future<User?> _signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      if (googleUser == null) {
+        // The user canceled the sign-in
+        return null;
+      }
+
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+      return userCredential.user;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,48 +43,74 @@ class AuthGate extends StatelessWidget {
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
-          return SignInScreen(
-            providers: [
-              EmailAuthProvider(),
-              GoogleProvider(
-                  clientId:
-                      "422436439009-pcmnvstk1ffs4l51c1d4e4o72kmn741u.apps.googleusercontent.com")
-            ],
-            headerBuilder: (context, constraints, shrinkOffset) {
-              return Padding(
-                padding: const EdgeInsets.all(20),
-                child: AspectRatio(
-                  aspectRatio: 1,
-                  child: Image.asset('assets/nnb.jpeg'),
-                ),
-              );
-            },
-            subtitleBuilder: (context, action) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: action == AuthAction.signIn
-                    ? const Text('Welcome to Nuts&Bolts, please sign in!')
-                    : const Text('Welcome to Nuts&Bolts, please sign up!'),
-              );
-            },
-            footerBuilder: (context, action) {
-              return const Padding(
-                padding: EdgeInsets.only(top: 16),
-                child: Text(
-                  'By signing in, you agree to our terms and conditions.',
-                  style: TextStyle(color: Colors.grey),
-                ),
-              );
-            },
-            sideBuilder: (context, shrinkOffset) {
-              return Padding(
-                padding: const EdgeInsets.all(20),
-                child: AspectRatio(
-                  aspectRatio: 1,
-                  child: Image.asset('assets/nnb.jpeg'),
-                ),
-              );
-            },
+          return Scaffold(
+            body: Center(
+              child: Column(
+                children: [
+                  Container(
+                    height: 100,
+                    decoration: BoxDecoration(
+                        color: MyColours.primaryColour,
+                        borderRadius: const BorderRadius.only(
+                            bottomRight: Radius.circular(50))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(25),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 40),
+                        Container(
+                          height: 200,
+                          child: Image.asset('assets/images/logo.png'),
+                        ),
+                        Text(
+                          'nuts&bolts.',
+                          style: TextStyle(
+                              color: MyColours.primaryColour,
+                              fontFamily: "Poppins",
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        const Text(
+                          'Welcome to nuts&bolts, sign in and start generating income by showing off your hands on skills!',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                padding: const EdgeInsets.all(10),
+                                foregroundColor: Colors.white,
+                                backgroundColor: MyColours.primaryColour,
+                                shape: ContinuousRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20))),
+                            onPressed: () async {
+                              User? user = await _signInWithGoogle();
+                              if (user != null) {
+                                // User successfully signed in
+                                // You can navigate to another screen if necessary
+                              }
+                            },
+                            child: const Text(
+                              'Sign in with Google',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        const Text(
+                          'By signing in, you agree to our terms and conditions.',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           );
         }
 
